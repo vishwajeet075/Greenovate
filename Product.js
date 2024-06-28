39397987762c464b581e8103f9087580803463cf
vishwajeet075 <158187016+vishwajeet075@users.noreply.github.com>
2024-06-28 12:05:39 +0530

    Update Product.js

--- Product.js
+++ Product.js
@@ -61,37 +61,39 @@ function toggleMenu() {
 
 
 
+
 document.addEventListener("DOMContentLoaded", function() {
-  const navbarMenu = document.querySelector('.navbar-menu');
-  const underline = document.querySelector('.underline');
-  const navItems = document.querySelectorAll('.navbar-menu ul li a:not(.contact-button)');
-  const dropdownMenu = document.querySelector('.navbar-dropdown');
-  const dropdownUnderline = document.querySelector('.underline-dropdown');
-  const dropdownNavItems = document.querySelectorAll('.navbar-dropdown nav ul li a');
+    const navbarMenu = document.querySelector('.navbar-menu');
+    const underline = document.querySelector('.underline');
+    const navItems = document.querySelectorAll('.navbar-menu ul li a:not(.contact-button)');
+    const dropdownMenu = document.querySelector('.navbar-dropdown');
+    const dropdownUnderline = document.querySelector('.underline-dropdown');
+    const dropdownNavItems = document.querySelectorAll('.navbar-dropdown nav ul li a');
 
-  const currentPage = window.location.pathname;
+    const currentPage = window.location.pathname.split('/').pop();
 
-  function setUnderline(item, underlineElement) {
-      const itemRect = item.getBoundingClientRect();
-      const parentRect = item.closest('nav').getBoundingClientRect();
-      underlineElement.style.width = `${itemRect.width}px`;
-      underlineElement.style.left = `${itemRect.left - parentRect.left}px`;
-  }
+    function setUnderline(item, underlineElement) {
+        const itemRect = item.getBoundingClientRect();
+        const parentRect = item.closest('nav').getBoundingClientRect();
+        underlineElement.style.width = `${itemRect.width}px`;
+        underlineElement.style.left = `${itemRect.left - parentRect.left}px`;
+    }
 
-  const currentNavItem = Array.from(navItems).find(item => {
-      const itemPath = new URL(item.href).pathname;
-      return itemPath === currentPage;
-  });
+    const currentNavItem = Array.from(navItems).find(item => {
+        const itemPath = item.getAttribute('href').split('/').pop();
+        return itemPath === currentPage || (currentPage === '' && itemPath === '#about');
+    });
 
-  const currentDropdownNavItem = Array.from(dropdownNavItems).find(item => {
-      const itemPath = new URL(item.href).pathname;
-      return itemPath === currentPage;
-  });
+    const currentDropdownNavItem = Array.from(dropdownNavItems).find(item => {
+        const itemPath = item.getAttribute('href').split('/').pop();
+        return itemPath === currentPage || (currentPage === '' && itemPath === '#about');
+    });
 
-  if (currentNavItem) {
-      setUnderline(currentNavItem, underline);
-  }
-
+    if (currentNavItem) {
+        setUnderline(currentNavItem, underline);
+    } else if (currentDropdownNavItem) {
+        setUnderline(currentDropdownNavItem, dropdownUnderline);
+    }
   navItems.forEach(item => {
       item.addEventListener('mouseover', function() {
           setUnderline(item, underline);
@@ -110,28 +112,4 @@ document.addEventListener("DOMContentLoaded", function() {
       });
   });
 
-  function toggleMenu() {
-      dropdownMenu.classList.toggle('active');
-  }
-
-  document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
-
-  dropdownNavItems.forEach(item => {
-      item.addEventListener('click', function() {
-          setUnderline(item, dropdownUnderline);
-          dropdownMenu.classList.remove('active');
-      });
-
-      item.addEventListener('mouseover', function() {
-          setUnderline(item, dropdownUnderline);
-      });
-
-      item.addEventListener('mouseleave', function() {
-          if (currentDropdownNavItem) {
-              setUnderline(currentDropdownNavItem, dropdownUnderline);
-          } else {
-              dropdownUnderline.style.width = '0';
-          }
-      });
-  });
 });
